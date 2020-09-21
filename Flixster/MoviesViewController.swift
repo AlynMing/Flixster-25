5aab33b304bec7b2209cc4ac27036244d6bc5337
Alex Long <alelong@ucdavis.edu>
2020-09-20 17:43:47 -0700

    Updated labels with their respective information

--- Flixster/MoviesViewController.swift
+++ Flixster/MoviesViewController.swift
@@ -46,12 +46,14 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
         
         let movie = movies[indexPath.row]
         let title = movie["title"] as! String
+        let synopsis = movie["overview"] as! String
         
-        cell.textLabel!.text = title
+        cell.titleLabel.text = title
+        cell.synposisLabel.text = synopsis
         
         return cell
     }
